068ce11a26c790a1c84f6c2b16ef5213e2e6b7a8
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-10-24 10:43:42 -0500

    now using view resource to fetch the posts

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-<script src='/js/main.js'></script>
 <script src='/js/vue.js'></script>
+<script src="https://cdn.jsdelivr.net/vue.resource/1.0.3/vue-resource.min.js"></script>
+<script src='/js/main.js'></script>
