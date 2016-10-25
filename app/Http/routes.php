c5660dfb742c8b1dac307f9633b284f3618e5ca9
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-10-25 09:24:31 -0500

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,11 +28,5 @@ Route::get('/', 'PostsController@welcome');
 // });
 Route::get('/home', 'PostsController@home');
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+
