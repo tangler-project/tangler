30cfb157495eda2ddb3711d1505e5d3cc37a6e76
Michael Truong <michael.d.truong@icloud.com>
2016-10-25 19:05:45 -0500

    css changes to change group view, fix bug where scroll was undefined on welcome view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,8 @@
 		<input class='form-control' type='email' name='email' placeholder='Your Email'>
 		<input class='form-control' type='password' name='password' placeholder='Your Password'>
 		<button type='submit' class='btn loginButton'>Log in</button>
-		<button type='submit' class='btn btn-primary loginButton'>Facebook</button>
-		<button type='submit' class='btn btn-warning loginButton'>Google</button>
+{{-- 		<button type='submit' class='btn btn-primary loginButton'>Facebook</button>
+		<button type='submit' class='btn btn-warning loginButton'>Google</button> --}}
 		<div class='closeNbarGuest'>X</div>
 	</form>
 </div>
