7d9da45530f23a9aedfa1eb9075ec82f0f80db9f
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-26 13:51:17 -0500

    Initial Commit:
    	faFaIcon contains data for a partial. I'm unsure where you want to inject the code but you can grab it from here and inject the code where necessary.

---
+++ public/faFaIcon.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		<link rel="shortcut icon" href="/img/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/tanglerIcon.ico/apple-icon-180x180.png">
+		
+		<link rel="manifest" href="/img/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	<title>Tangle Your Network!!!</title>
+	</head>
+	
+	
