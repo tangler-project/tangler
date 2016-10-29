25c403831e73e334c44556f21b474d674e1986e9
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-10-29 14:38:05 -0500

    delete unused files and added a search bar in public guest view that filters the public groups on screen

--- resources/views/scripts/public-knots-script.blade.php
+++ resources/views/scripts/public-knots-script.blade.php
@@ -140,6 +140,23 @@
 
 	});
 
+	//search bar
+	var $searchBar = $('#searchBar');
+	$searchBar.keyup(function() {
+		
+		$rows = $('#content div');
+
+	    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+	        reg = RegExp(val, 'i'),
+	        text;
+	    
+	    $rows.show().filter(function() {
+	        text = $(this).text().replace(/\s+/g, ' ');
+	        return !reg.test(text);
+	    }).hide();
+	});
+	//end 
+
 
 </script>
 
