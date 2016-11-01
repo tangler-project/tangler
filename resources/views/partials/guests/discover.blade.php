635a3821964a1a94ab2ac8497e1b9d4c593d8298
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-11-01 16:59:30 -0500

    Update: Added content to the Create, Connect and Unite divs.

--- resources/views/partials/guests/discover.blade.php
+++ resources/views/partials/guests/discover.blade.php
@@ -10,21 +10,15 @@
 	<div class='discoverRight'>
 		<div class='discoverContent' id='discoverContent1'>
 			<h3>Create</h3>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Strengthen your connections by creating Knots and extending threads for others to join your knots. 
 		</div>
 		<div class='discoverContent' id='discoverContent2'>
 			<h3>Connect</h3>
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			Sew new friendships and stay connected with what's important to you. Join your friends knots or create a knot of your own.
 		</div>
 		<div class='discoverContent' id='discoverContent3'>
 			<h3>Unite</h3>
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			Unite for popular causes and grow your community.  Whether you have a non-profit or a fundraiser it's easy to get people involved and unite them for your events.
 		</div>
 		<h4 class='actionDivBlue' v-on:click="showSignUp">Sign Up</h4>
 	</div>
