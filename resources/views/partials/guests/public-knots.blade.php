4061f787244ea0b59237d59f3b4e58a23fa54d94
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-11-02 20:03:12 -0500

    Update:

--- resources/views/partials/guests/public-knots.blade.php
+++ resources/views/partials/guests/public-knots.blade.php
@@ -3,7 +3,7 @@
 		<div class='container-fluid landingLeft' v-on:mouseover='noScroll' v-on:mouseleave='yesScroll'>
 
 			<div class='publicKnotParent' v-for="group in groups" id="content">
-				<div class='publicKnot' v-on:click="goToPost(group)"><img class='groupBanner' src="http://placehold.it/1400x425">
+				<div class='publicKnot' v-on:click="goToPost(group)"><img class='groupBanner' v-bind:src="group.img_url">
 					<div class='groupName'>
 						@{{group.title}} @{{group.id}}
 					</div>
