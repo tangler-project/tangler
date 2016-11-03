c1b42d70b42a51d16069d611b07f5582067b755e
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-11-03 15:56:51 -0500

    added errors that do not show on reload for sign up, and the errors on reload for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,10 @@
 <div class='nbarGuestLogin'>
 	<form method='POST' action="{{ action('Auth\AuthController@postLogin') }}">
 		{{ csrf_field() }}
-		<input class='form-control' type='email' name='email' placeholder='Your Email'>
-		<input class='form-control' type='password' name='password' placeholder='Your Password'>
+		<input class='form-control' type='email' name='email' placeholder='Your Email' required>
+		<input class='form-control' type='password' name='password' placeholder='Your Password' required>
 		<button type='submit' class='btn loginButton'>Log in</button>
 	</form>
+	@include('errors.login-register')
 </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,16 @@
 <div class='nbarGuestSignup'>
 	<form method='POST' action="{{ action('Auth\AuthController@postRegister') }}">
 		{{ csrf_field() }}
-		<input class='form-control' type='text' name='name' placeholder='Your Name'>			
-		<input class='form-control' type='email' name='email' placeholder='Your Email'>
-		<input class='form-control' type='password' name='password' placeholder='Your Password' v-model="newUser.password">
-		<input class='form-control' type='password' name='password_confirmation' placeholder='Confirm Password' v-model="newUser.confirmPassword">
+		<input class='form-control' type='text' name='name' placeholder='Your Name' required>			
+		<input class='form-control' type='email' name='email' placeholder='Your Email' required>
+		<input class='form-control' type='password' name='password' placeholder='Your Password' v-model="newUser.password" required>
+		<input class='form-control' type='password' name='password_confirmation' placeholder='Confirm Password' v-model="newUser.confirmPassword" required>
 
 		<button type='submit' class='btn signupButton' v-on:click="validateARegister">Get Tangled</button><br>
 {{-- 		<button type='submit' class='btn btn-primary loginButton'>Facebook</button>
 		<button type='submit' class='btn btn-warning loginButton'>Google</button> --}}
 	</form>
 	<div v-show="errorPassword">
-		<p>The passwords entered do not match</p>
+		<p class="errors">The passwords entered do not match</p>
 	</div>
 </div>
