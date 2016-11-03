270f63387fdb5f2bea3d8663c94efb201d6e74d3
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-11-03 10:01:36 -0500

    Update: Updated the file picker for fileStack to set min and max dimensions giving users the freedom to select the size of image they wish to use.

--- resources/views/partials/users/change-knot.blade.php
+++ resources/views/partials/users/change-knot.blade.php
@@ -1,18 +1,18 @@
 
-<div class='container-fluid changeGroupView'>
+<div class='changeGroupView'>
 
-	<div class='container-fluid changeGroupLeft'>
+	<div class='changeGroupLeft'>
 		<div class='publicKnotParent' v-for="group in groups">
-			<div class='publicUserKnot' v-on:click="goToPost(group,'.topNbarUserPublic')"><img class='groupBanner' src="http://placehold.it/1400x425">
+			<div class='publicUserKnot' v-on:click="goToPost(group,'.topNbarUserPublic')"><img class='groupBanner' v-bind:src="group.img_url">
 				<div class='groupName'>
 					@{{group.title}} @{{group.id}}
 				</div>
 			</div>
 		</div>	
 	</div>
-	<div class='container-fluid changeGroupRight'>
+	<div class='changeGroupRight'>
 		<div v-for="group in privateGroups">
-			<div class='privateKnot' v-on:click="goToPost(group,'.topNbarUser')"><img class='groupBanner' src="http://placehold.it/1400x425">
+			<div class='privateKnot' v-on:click="goToPost(group,'.topNbarUser')"><img class='groupBanner' v-bind:src="group.img_url">
 				<div class='groupName'>
 					@{{group.title}} @{{group.id}}
 				</div>
@@ -23,42 +23,50 @@
 </div>
 
 
-<div class='container-fluid publicUserGroupView'>
+<div class='publicUserGroupView'>
 	<div class='publicUserGroupLeft' id='userPosts'>
-		<div class='posts list-group'>
-			<div v-for="post in groupPosts">
-				<h5>@{{ post.user.name }}</h5>
-				<p class="outputText" onchange="showEmoji()">@{{post.content}}<p>
-				<img v-bind:src="post.img_url" alt=""><br>
-				<strong>@{{post.created_at}}</strong>
+		{{-- <div class='posts list-group'> --}}
+			<div class='posts' v-for="post in groupPosts">
+				<div class="outputText" onchange="showEmoji()">@{{post.content}}
+					<div class='avatarDiv'>
+						<img class='avatarImg' v-bind:src="post.user.img_url" alt="">
+					</div>
+					<div class='usernameDiv'>
+						@{{ post.user.name }}
+					</div>
+				{{-- <img v-bind:src="post.img_url" alt=""> --}}
+					{{-- BEGIN THUBS UP-DOWN--}}
+					<div class='thumbsParent'>
+						<form method="POST" class="likeForm">
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+							<input id="postVote" type="" name="vote" value="1" v-model="post.vote" hidden>
+							<input id="postIdUp" type="" name="post_id" value=@{{post.id}} v-model="postId" hidden>
+
+							<button type="submit" class="thumbsBtn" v-on:click="setVotesUp($event, post.id)" >
+								<i class="fa fa-thumbs-o-up" aria-hidden="true" > @{{post.likes}}</i>
+							</button>
+						</form>
+						<form method="POST" class="dislikeForm">
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+							<input id="postVote" type="" name="vote" value="-1" v-model="post.vote" hidden>
+							<input id="postIdDown" type="" name="post_id" value=@{{post.id}} v-model="postId" hidden>
+
+							<button type="submit" class="thumbsBtn" v-on:click="setVotesDown($event, post.id)" >
+								<i class="fa fa-thumbs-down" aria-hidden="true"> @{{post.dislikes}}</i>
+							</button>	
+						</form>
+					</div>
+					{{-- END --}}
+					<div class='postDate'>@{{post.created_at}}</div>
+				</div>
 				
-				{{-- BEGIN THUBS UP-DOWN--}}
-				<form method="POST" class="pull-left">
-					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<input id="postVote" type="" name="vote" value="1" v-model="post.vote" hidden>
-					<input id="postIdUp" type="" name="post_id" value=@{{post.id}} v-model="postId" hidden>
-
-					<button type="submit" class="btn btn-default btn-md btn-thumbs" v-on:click="setVotesUp($event, post.id)" >
-						<i class="fa fa-thumbs-o-up" aria-hidden="true" >@{{post.likes}}</i>
-					</button>
-				</form>
-				<form method="POST" >
-					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<input id="postVote" type="" name="vote" value="-1" v-model="post.vote" hidden>
-					<input id="postIdDown" type="" name="post_id" value=@{{post.id}} v-model="postId" hidden>
-
-					<button type="submit" class="btn btn-default btn-md btn-thumbs" v-on:click="setVotesDown($event, post.id)" >
-						<i class="fa fa-thumbs-down" aria-hidden="true">@{{post.dislikes}}</i>
-					</button>	
-				</form>
-				{{-- END --}}
 			</div>
-		</div>	
+		{{-- </div>	 --}}
 	</div>
 			
 
 	<div class='createNewPost'>
-		<form method='POST'>
+		<form method='POST' class='postForm'>
 			{{-- {{ csrf_field() }} --}}
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			
@@ -67,7 +75,7 @@
 
 			<input type="hidden" name="img_url" id="uploadedImage" value="" v-model="post.img_url">
 			{{-- FILESTACK --}}
-			<input type="filepicker-dragdrop" data-fp-button-text="Add Photo" onchange="showImage();" data-fp-multiple="false" data-fp-crop-dim="230,230" data-fp-apikey="AHtuHxJJyS2ijt2rx4ZH1z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-multiple="false" onchange="out='';for(var i=0;i<event.fpfiles.length;i++){out+=event.fpfiles[i].url;out+=' '};alert(out)">
+			<input type="filepicker-dragdrop" data-fp-button-text="Add Photo" onchange="showImage();" data-fp-multiple="false"  data-fp-crop-min="600,600" data-fp-crop-max="1200, 1200" data-fp-apikey="AHtuHxJJyS2ijt2rx4ZH1z" data-fp-mimetypes="image/*" data-fp-container="modal" data-fp-multiple="false" onchange="out='';for(var i=0;i<event.fpfiles.length;i++){out+=event.fpfiles[i].url;out+=' '};alert(out)">
 			{{-- END FILESTACK --}}
 
 			<button type='submit' hidden v-on:click="savePost">Post</button>
@@ -75,22 +83,24 @@
 	</div>
 
 	<div class='publicUserGroupRight'>
-		<div class="list-group listOfEvents">
+		<div class="listOfEvents">
 			<button class='btn createEventButton' v-on:click="showCreateEvent">New Event</button>
 			<div v-for="event in groupEvents">
-			  	<div class="list-group-item list-group-item-action">
+				<div class='eventContainer'>
 			  		<div v-if="event.owner_id == {{Auth::user()->id}}">
 				  		<button class='btn' v-on:click="goToEvent(event)">Edit</button>
 			  		</div>
-				  	<h5>@{{event.title}}</h5>
-				  	<p>@{{event.content}}</p>
-				  	<img v-bind:src="event.img_url" alt=""><br>
-				  	<span>Starts: </span><strong>@{{event.start_date}}</strong>
-				  	<span>Ends: </span><strong>@{{event.end_date}}</strong>
-
-			  	</div>
+				  	<div class='eventContentDiv'>@{{event.content}}
+					  	<div class='eventTitleDiv'><strong>@{{event.title}}</strong></div>
+					  	{{-- <img v-bind:src="event.img_url" alt=""><br> --}}
+					  	<div class='eventBannerDiv'>
+						  	<img class='eventBannerImg' src="http://placehold.it/400x200">
+						</div>
+					  	<div class='eventDates'>@{{event.start_date}}@{{event.end_date}}</div>
+				  	</div>
+				 </div>
 			</div>
-	  	</div>
-	  	
+	  	</div> 	
 	</div>
+
 </div>
