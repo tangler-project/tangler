324fb854de016502376373ce0321af42ea5ab5f9
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-11-05 17:15:52 -0500

    added the navbar icon

--- resources/views/partials/users/navbar-user.blade.php
+++ resources/views/partials/users/navbar-user.blade.php
@@ -174,6 +174,12 @@
 		<div class='topLinkSeperator'>/</div>
 		<div class='guestTopLinkUser linkCreateKnot' v-on:click="showEditProfile">@{{user.name}}</div>
 	</div>
-	<div class='topNbarTab'>Navigation</div>
+	<div class='topNbarTab'>
+		
+		Navigation
+	
+			
+		<img class="img-navbar" src="http://www.gigabyte.us/WebPage/-54/img/arrow_down.png" alt="">
+	</div>
 </div>
 
