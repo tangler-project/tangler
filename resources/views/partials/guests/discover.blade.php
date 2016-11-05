1f8fe9258ab78e3ef185811f28156dcd74f07372
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-11-05 18:07:50 -0500

    added more content to the discover page

--- resources/views/partials/guests/discover.blade.php
+++ resources/views/partials/guests/discover.blade.php
@@ -23,7 +23,7 @@
 		<div id='discoverContent1'>
 			<div class='discoverContent'>
 				<div class='discoverContentTitle'>Create</div>
-				Strengthen your connections by creating Knots and extending threads for others to join your knots.
+				  Strengthen your connections by creating Knots(groups) and extending threads for others to join your knots.  Users can create Knots for others to join via a private network. We use password protection that strengthens connections through your experience with tanglr
 				<h4 class='actionDivBlue' v-on:click="showSignUp">Sign Up</h4>
 				<div class='downArrow' v-on:click='arrowScroll'>
 					{{-- &#10151 --}}
@@ -34,7 +34,9 @@
 		<div id='discoverContent2'>
 			<div class='discoverContent'>
 				<div class='discoverContentTitle'>Connect</div>
-				Sew new friendships and stay connected with what's important to you. Join your friends knots or create a knot of your own.
+				Sew new friendships and connect with interactive, realtime chats/posts and customize your experience with avatar images and adding images from your connections in our media area.  Did we mention access to your photos on Facebook, Instagram, and Google? Oh yeah!
+
+                
 				<h4 class='actionDivBlue' v-on:click="showSignUp">Sign Up</h4>
 				<div class='downArrow' v-on:click='arrowScroll'>
 					{{-- &#10151 --}}
@@ -50,7 +52,9 @@
 				</div>
 				<br><br>
 				<div class='discoverContentTitle'>Unite</div>
-				Unite for popular causes and grow your community.  Whether you have a non-profit or a fundraiser it's easy to get people involved and unite them for your events.
+				Unite for popular causes and grow your community whether you have a dinner, non-profit or a fundraiser it's easy to get people involved and unite them for your events.
+
+                Build your movement with Tanglr below!!!
 				<h4 class='actionDivBlue' v-on:click="showSignUp">Sign Up</h4>
 			</div>
 		</div>
