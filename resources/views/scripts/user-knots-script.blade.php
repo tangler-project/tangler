1692257a47a1433994ea45d32d65a02b10e309e5
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-11-05 17:50:35 -0500

    testing to show description

--- resources/views/scripts/user-knots-script.blade.php
+++ resources/views/scripts/user-knots-script.blade.php
@@ -9,6 +9,7 @@
 		data: function(){
 
 			return {
+				
 
 				//time in ms to close navbar after action
 				timeNavClose:550,
@@ -99,7 +100,10 @@
 
 
 		methods:{
-			
+			showDescriptionGroup: function(group){
+				console.log(group);
+				this.mouseOverDiv = true;
+			},
 			
 			setVotesDown: function(e, postId){
 				e.preventDefault();
@@ -891,6 +895,7 @@
 			},
 
 			mouseInRight: function(){
+				
 				this.mouseLeft = false;
 				if(this.menuState == false){
 					$('.leftSideTab').stop().animate({
@@ -908,9 +913,11 @@
 							left: '-30px'
 						}, 500);
 				}
+				
 			},
 
 			mouseInLeft: function(){
+
 				this.mouseLeft = true;
 				if(this.menuState == false){
 					$('.leftSideTab').stop().animate({
@@ -928,6 +935,8 @@
 							right: '-30px'
 						}, 500);
 				}
+
+
 			},
 
 			//Pusher start
