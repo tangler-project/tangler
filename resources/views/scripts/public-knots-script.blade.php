5a1e2821345226980dad57f21f63e710893e0ede
Michael Truong <michael.d.truong@icloud.com>
2016-11-07 22:16:44 -0600

    many bug fixes for mobile responsiv

--- resources/views/scripts/public-knots-script.blade.php
+++ resources/views/scripts/public-knots-script.blade.php
@@ -133,6 +133,9 @@
 				$('.topNbarHover').stop().animate({
 					top: '-42px'
 				}, this.navbarTransitionSpeed);
+				$('.mobileNbar').stop().animate({
+					top: '0px'
+				}, this.navbarTransitionSpeed);
 				$('.landingRight').stop().animate({
 					right: '0px'
 				}, this.navbarTransitionSpeed);
@@ -216,7 +219,7 @@
 						opacity: '0'
 					}, 600);
 					setTimeout(function(){
-						$('.topNbarHover').stop().animate({
+						$('.topNbarHover, .mobileNbar').stop().animate({
 							top: '-100px'
 						}, 700);
 						$('.landingRight').stop().animate({
@@ -243,7 +246,7 @@
 						$('.cover').css('pointer-events', 'auto');
 					}, 1300);
 				} else {
-					$('.topNbarHover').stop().animate({
+					$('.topNbarHover, .mobileNbar').stop().animate({
 						top: '-100px'
 					}, this.navbarTransitionSpeed);
 					$('.landingRight').stop().animate({
@@ -308,10 +311,17 @@
 					$('.landingLeft').css('opacity', '0');
 					$('.landingRight').css('top', '-200');
 					$('.landingLeft').css('top', '200');
-					$('.landingLeft').stop().animate({
-						top: '0px',
-						opacity: '1'
-					}, this.pageTransitionSpeed);
+					if(this.mobileVersion == true){
+						$('.landingLeft').stop().animate({
+							top: '40px',
+							opacity: '1'
+						}, this.pageTransitionSpeed);
+					} else if(this.mobileVersion == false){
+						$('.landingLeft').stop().animate({
+							top: '0px',
+							opacity: '1'
+						}, this.pageTransitionSpeed);	
+					}
 					$('.landingRight').stop().animate({
 						top: '0px',
 						opacity: '1'
@@ -340,11 +350,18 @@
 					$('.landingLeft').css('top', '200px');
 					$('.landingRight').css('top', '-200px');
 					$('.landingView').css('display', 'flex');
+					if(this.mobileVersion == true){
+						$('.landingLeft').stop().animate({
+							top: '40px',
+							opacity: '1'
+						}, this.pageTransitionSpeed);
+					} else if(this.mobileVersion == false){
+						$('.landingLeft').stop().animate({
+							top: '0px',
+							opacity: '1'
+						}, this.pageTransitionSpeed);	
+					}
 					$('.landingRight').stop().animate({
-						top: '0px',
-						opacity: '1'
-					}, this.pageTransitionSpeed);
-					$('.landingLeft').stop().animate({
 						top: '0px',
 						opacity: '1'
 					}, this.pageTransitionSpeed);
